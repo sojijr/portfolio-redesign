3d118be73d3a562b46230c1716519f412bfb6538
Igwedinma Ifechukwu Divine <64442300+I-Divine@users.noreply.github.com>
2024-01-22 22:15:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
